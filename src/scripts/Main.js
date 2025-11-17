3e9d02d9d67adc4e46a7531b6b58602d1438e4b0
GabbDup <gabriel.dupras01@gmail.com>
2025-11-17 12:49:29 -0500

    manque juste le footer et les spans

--- src/scripts/Main.js
+++ src/scripts/Main.js
@@ -1,12 +1,25 @@
 import Icons from './utils/Icons.js';
+import Swiper from 'swiper/bundle';
 
 class Main {
   constructor() {
     this.init();
   }
 
+  initSwiper() {
+    const target = document.querySelector('.js-swiper');
+    const swiper = new Swiper(target, {
+      speed: 400,
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    });
+  }
+
   init() {
     Icons.load();
+    this.initSwiper();
   }
 }
 new Main();
